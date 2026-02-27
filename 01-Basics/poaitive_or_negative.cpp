1ba4d76c974eebd3f5f0e928aa43a3b6b065fcac
Pavithra gowda <pavithragowda954@gmail.com>
2026-02-27 23:23:45 +0530

    Check if a number is Positive/Negative/Zero

---
+++ 01-Basics/poaitive_or_negative.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter a number:";
+    cin>>number;
+    if(number>0)
+        cout<<"the number is positive.";
+    else if(number<0)
+        cout<<"the number is negetive.";
+    else
+        cout<<"the number is zero";
+    
+    return 0;
+
+
+
+}
