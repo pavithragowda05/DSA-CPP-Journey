71cff066a7df4bfd0a40c64b045016896fedd4eb
Pavithra gowda <pavithragowda954@gmail.com>
2026-02-28 21:07:02 +0530

    Printed numbers from 1 to N using while loop

---
+++ 01-Basics/print_1_to_n.cpp
@@ -0,0 +1,14 @@
+// Print 1 to N
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    i=1;
+    cin>>n;
+    while (i!=n+1)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    
+}
