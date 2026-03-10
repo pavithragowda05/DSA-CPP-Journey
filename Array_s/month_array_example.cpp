dad78cd42cfeed49483f5dd02fe3f4e86adad7f4
Pavithra gowda <pavithragowda954@gmail.com>
2026-03-10 20:28:06 +0530

    Added C++ program to print third month using array indexing

---
+++ Array_s/month_array_example.cpp
@@ -0,0 +1,16 @@
+// Problem: Print the third month from the array
+// Platform: CodeChef Practice
+// Language: C++
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string months[4] = {"January", "February", "March", "April"};
+
+    cout << months[2] << endl;
+
+    return 0;
+}
