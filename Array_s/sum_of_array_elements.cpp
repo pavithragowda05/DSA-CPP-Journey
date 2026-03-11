f40986467b4fb71edd452ee456476abec23dfd1f
Pavithra gowda <pavithragowda954@gmail.com>
2026-03-11 21:01:56 +0530

    Added C++ program to calculate sum of array elements

---
+++ Array_s/sum_of_array_elements.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Find the sum of elements in an array
+Concept: Arrays
+Language: C++
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
