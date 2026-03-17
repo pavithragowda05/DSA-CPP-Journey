ab27c485a2bc94dabdc6f593d5c255922222acfc
Pavithra gowda <pavithragowda954@gmail.com>
2026-03-17 20:28:27 +0530

    Create matrix_addition.cppAdd matrix addition program in C++

---
+++ Array_s/matrix_addition.cpp
@@ -0,0 +1,30 @@
+// - Implemented matrix addition using 2D arrays
+// - Took input for two matrices of size n x m
+// - Printed the resulting matrix after addition
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,m;
+	cin>>n>>m;
+	int arr1[n][m],arr2[n][m],arr3[n][m];
+	for(int i=0;i<n;i++){
+	    for(int j=0;j<m;j++){
+	        cin>>arr1[i][j];
+	    }
+	}
+	for(int i=0;i<n;i++){
+	    for(int j=0;j<m;j++){
+	        cin>>arr2[i][j];
+	    }
+	}    
+	for(int i=0;i<n;i++){
+	    for(int j=0;j<m;j++){
+	        arr3[i][j]=arr1[i][j]+arr2[i][j];
+	        cout<<arr3[i][j]<<" ";
+	    }
+	    cout<<"\n";
+	}
+
+}
