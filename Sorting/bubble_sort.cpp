b9306faf3b6c8bac5da24dab06b1df640713008b
Pavithra gowda <pavithragowda954@gmail.com>
2026-05-23 11:29:40 +0530

    Updated bubble sort program

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -14,32 +14,46 @@
     - Best Case: O(n)
 */
 
+// File Name: bubble_sort.cpp
+
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
-                int temp = arr[j];
+int main()
+{
+    int n, temp;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Bubble Sort
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
-}
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
+    cout << "Sorted array:\n";
+
+    for(int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
-    cout << endl;
-}
+    }
 
-int main() {
-    int arr[] = {6, 5, 3, 1, 8, 7, 2, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n);
-    printArray(arr, n);
     return 0;
 }
